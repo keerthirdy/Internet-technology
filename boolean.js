f462cc2d56cbfc2c31b0b1574cd305663ef62a26
keerthireddy <keerthireddypyk@gmail.com>
2024-03-28 15:15:50 +0530

    boo

---
+++ boolean.js
@@ -0,0 +1,8 @@
+let isMore = 2 >= 4; // false
+let isLess = 5 < 10; // true
+let bool1 = isMore || isLess; // true
+let bool2 = isMore && isLess; // false
+let isEqual1 = 0 == '0';//true type conversion
+let isEqual2 = 0 === '0; //false no type conversion
+let notEqual1 = 0 != '0'; //false
+let notEqual2 = 0 !== '0'; //true
